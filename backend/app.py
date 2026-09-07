9c269f8f2db0034252bdf2a9ea8a6d806e445670
binyul1 <jiibinyul@gmail.com>
2026-09-07 19:51:54 +0545

    Fix typos and improve response structure in API routes and tests

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ DOG_API = "https://dog.ceo/api/breeds/image/random"
 
 @app.route("/")
 def home():
-    return "Welcom to the public API Demo"
+    return "welcome to the Public API Demo"
 
 @app.route("/dog")
 def dog():
@@ -16,17 +16,18 @@ def dog():
         data = response.json()
         return jsonify({
             "status": data["status"],
-            "message": data["message"]
+            "image_url": data["message"]
         })
     else:
         return jsonify({"error": "Could not fetch dog image"}), 500
-    
+
 @app.route("/dog/html")
 def dog_html():
-    response = requests.get(DOG_API)
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
     if response.status_code == 200:
         data = response.json()
-        return f"<h1>Random Dog</h1><img src='{data{'message'}}'/>"
+        return f"<h1>Random Dog</h1><img src='{data['message']}'/>"
 
-if __name__=="__main__"
-app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
